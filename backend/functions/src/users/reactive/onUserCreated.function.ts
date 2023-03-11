65492a6f3b7f9c31b8a9b098b234441d710c5f45
Usama Qazi <usamyharis@yahoo.com>
2023-03-11 23:09:52 +0500

    Initial commit

---
+++ backend/functions/src/users/reactive/onUserCreated.function.ts
@@ -0,0 +1,8 @@
+import * as functions from "firebase-functions";
+
+exports.onUserCreated = functions.firestore
+  .document("users/{userId}")
+  .onCreate((userSnapshot, context) => {
+    const data = userSnapshot.data();
+    console.log(`User created | send an email to ${data.email}`);
+  });
